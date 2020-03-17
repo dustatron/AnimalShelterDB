4f6b9a72460fc9bdc3f74e0f05d8e27cad992328
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-17 11:56:55 -0700

    display home page
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -24,7 +24,7 @@ namespace AnimalShelter
     {
       services.AddMvc();
       services.AddEntityFrameworkMySql()
-        .AddDbContext<AnimalContext>(options => options
+        .AddDbContext<AnimalShelterContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
     }
 
